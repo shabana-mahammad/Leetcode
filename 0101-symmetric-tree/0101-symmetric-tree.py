1ac420d58245afb6710d1aeb43833adf7b48363c
SHABANA MAHAMMAD <136444514+shabana-mahammad@users.noreply.github.com>
2024-12-01 23:59:20 +0530

    Time: 0 ms (100%), Space: 17.3 MB (5.12%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -7,15 +7,17 @@
 from collections import deque
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        if root== None:
+        if not root:
             return True
-        return self.isSymmerticsHelp(root.left,root.right)
-
-    def isSymmerticsHelp(self,left,right):
-        if not left and not right:
-            return True
-        if not left or not right:
-            return False
-        if left.val!=right.val:
-            return False
-        return (self.isSymmerticsHelp(left.left, right.right) and self.isSymmerticsHelp(left.right, right.left))
+        queue = deque([(root.left, root.right)])
+        while queue:
+            t1, t2 = queue.popleft()  
+            if not t1 and not t2: 
+                continue
+            if not t1 or not t2:  
+                return False
+            if t1.val != t2.val:  
+                return False
+            queue.append((t1.left, t2.right))
+            queue.append((t1.right, t2.left))
+        return True
